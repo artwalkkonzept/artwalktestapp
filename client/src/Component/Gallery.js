8da8c704ea962f5419fd708b29c7779065b9c52c
@artwalkkonzept <artwalkkonzept@gmail.com>
2021-09-30 15:05:41 +0200

    new build deploy

--- client/src/Component/Gallery.js
+++ client/src/Component/Gallery.js
@@ -1,19 +1,13 @@
-import React from 'react';
-
-let urlm = {
-  imageUrl1:"http://prfrankild.dk/ArtWalk_Logo%20final_ww.png"
-}
-
-function Gallery(props) {
-  return (
-    <div className="background-blue">
-     
-
-<div className="img-with-text">
-      <img alt="" height="270" src={urlm.imageUrl1} />
+import React, { Component } from 'react';
+import logo from '../content/ArtWalk_Logo final_ww.png';
+export default class Header extends Component {
+  render() {
+    return (
+      <div className="row">
+        <div className="logo">
+          <img alt="" src={logo} width="270" />
         </div>
-  </div>
-);
-}
-
-export default Gallery;
+      </div>
+    );
+  }
+} 
